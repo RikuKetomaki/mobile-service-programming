133cc05ad1135dac676a3890b93422a20c0d7b63
RikuKetomaki <riku.ketomaki@gmail.com>
2024-02-15 10:54:18 +0200

    Added

---
+++ mini-yahzee/components/Gameboard.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import style from '../styles/Style';
+
+export default function Gameboard () {
+    
+    return (
+        <View >
+            <Text >Gameboard here</Text>
+        </View>
+    )
+}
---
+++ mini-yahzee/components/Scoreboard.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import style from '../styles/Style';
+
+export default function Scoreboard () {
+    
+    return (
+        <View >
+            <Text >Scoreboard here</Text>
+        </View>
+    )
+}
